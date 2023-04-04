f54089f6f1232e6f773c5214309bafec82a01690
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2023-04-04 08:53:21 +0300

    User class added

--- main.py
+++ main.py
@@ -60,3 +60,25 @@ if __name__ == "__main__":
             if not row:
                 db.store(extracted)
         time.sleep(2)
+
+'''
+Create a class named User. The class should:
+
+1 .have two methods, get_name and age.
+
+2. The two methods should contain only a pass statement inside them.
+
+3. The get_name method should contain a self parameter.
+
+4. The age method should contain a self and a current_year parameter.
+
+Solution: See the attached task1.py file in the Resources of the next lecture.
+'''
+
+
+class User:
+    def get_name(self):
+        pass
+
+    def age(self, current_year):
+        pass
